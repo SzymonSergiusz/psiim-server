ea2ca77360548176affa373ae6a6865427e24027
SzymonSergiusz <62117922+SzymonSergiusz@users.noreply.github.com>
2024-06-23 19:33:59 +0200

    +reqs

--- db/database.py
+++ db/database.py
@@ -1,10 +1,14 @@
+import os
+
 from sqlalchemy import create_engine
 from sqlalchemy.ext.declarative import declarative_base
 from sqlalchemy.orm import sessionmaker
 
 # sqlite for production
 # SQLALCHEMY_DATABASE_URL = 'sqlite:///./sql_app.db'
-SQLALCHEMY_DATABASE_URL = "postgresql://postgres:bazydanych@localhost/psiim-db"
+# SQLALCHEMY_DATABASE_URL = "postgresql://postgres:bazydanych@localhost/psiim-db"
+
+SQLALCHEMY_DATABASE_URL = os.getenv("DATABASE_URL", "postgresql://postgres:bazydanych@localhost/psiim-db")
 
 engine = create_engine(
     SQLALCHEMY_DATABASE_URL, echo=True
